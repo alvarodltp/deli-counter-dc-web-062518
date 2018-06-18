c33234bbd1c99a80e0d5211df30e2c26eb90774d
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-18 20:42:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
   
 def line(katz_deli)
   if katz_deli.length == 0 
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     str = "The line is currently:"
     katz_deli.each_with_index do |value, index|
